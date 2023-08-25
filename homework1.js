09c415a6f906f16553950b062135f4c281cc1b47
annak23003 <annchubich@gmail.com>
2023-08-25 15:05:55 +0200

    add_new_file_with_homework1

---
+++ homework1.js
@@ -0,0 +1,15 @@
+//variable with 'We are good' string
+let stringText = 'We are good'
+//output 2 variable values to the console together
+let stringOneHello = 'Hello'
+let stringTwoWorld = 'World'
+console.log(`${stringOneHello} ${stringTwoWorld}`)
+
+//checking the data type
+let myVariable = 'value'
+console.log(typeof myVariable)
+
+//getting the value of another variable
+//( `hello ${"name"}` ) - incorrect
+let name = 'Anna'
+console.log(`hello ${name}`)
