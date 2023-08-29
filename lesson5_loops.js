e6bc43a85784c611ff5f277364cd1865ae9fa679
annak23003 <annchubich@gmail.com>
2023-08-29 19:03:46 +0200

    updated_lesson5

--- lesson5_loops.js
+++ lesson5_loops.js
@@ -1,1 +1,17 @@
-let value1 = 'value'; 
+let num215 = 215;
+
+if (num215 == 215) {
+    console.log('yes. value under condition')
+    console.log('1')
+    console.log('2')
+};
+
+if (num215 >= 215 && num215 < 250) {
+    console.log('Second condition works')
+};
+
+if (num215 > 300) {
+    console.log('num215 varaible greater than 300')
+} else {
+    console.log('code goes to the else')
+}
