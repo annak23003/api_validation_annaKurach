ad2d53442e39a0ad28810715ccf7f156001d408f
annak23003 <annchubich@gmail.com>
2023-10-29 19:27:04 +0100

    lesson18

--- cypress/e2e/custom_scenarios/query_mongo.cy.js
+++ cypress/e2e/custom_scenarios/query_mongo.cy.js
@@ -10,4 +10,25 @@ describe('Find data', () => {
             });
         })
     })
+
+    context('UI & API request', () => {
+
+        it('create user', () => {
+            cy.request({
+                method: 'POST',
+                url: '/api/users/register',
+                headers: {
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTNhOWJmMDNkYTQ1NTlmYjkwZjdhZTYiLCJpYXQiOjE2OTgzNDMwMzUsImV4cCI6MTY5ODk0NzgzNX0.TZQzUcR5M0qDjhH7HmP5713sENp_KYM87sq0nCO8MzI'
+                },
+                body: {
+                    firstName: 'fff2',
+                    lastName: 'sss2',
+                    username: 'fff2',
+                    password: 'abc1234'
+                }
+            })
+        })
+    })
 })
+
+
